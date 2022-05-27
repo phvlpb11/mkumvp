a028fc2afbcb45219da72091a9c546efba6b3894
Lakshya <bhati.lakshya11@gmail.com>
2022-05-27 19:09:43 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+(function($){
+    $(document).ready(function(){
+$('.carousel').flickity();
+    });
+})(jQuery)
+
+// MDB Lightbox Init
+$(function () {
+    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+  });
